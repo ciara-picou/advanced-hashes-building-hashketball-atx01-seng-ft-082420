8dd3cda7c38ed243dcecb3e7427bf4251b01cc89
Ciara Picou <ciarapicou@att.net>
2020-07-30 04:07:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,8 +6,9 @@ def game_hash
       colors: ["Black", "White"], 
       players: [
                 {
-                  player_name: "Jeff Adrien",
-                  number: 4, shoe: 18,
+                  player_name: "Alan Anderson",
+                  number: 4, 
+                  shoe: 18,
                   points: 10, 
                   rebounds: 1, 
                   assists: 1, 
@@ -24,7 +25,8 @@ def game_hash
       players: [
                 {
                   player_name: "Jeff Adrien",
-                  number: 4, shoe: 18,
+                  number: 4, 
+                  shoe: 18,
                   points: 10, 
                   rebounds: 1, 
                   assists: 1, 
