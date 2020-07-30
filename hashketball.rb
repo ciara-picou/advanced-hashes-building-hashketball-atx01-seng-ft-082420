9e0163c8b1e8a129e8370e09440200b97fc95b28
Ciara Picou <ciarapicou@att.net>
2020-07-30 04:09:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,8 +15,22 @@ def game_hash
                   steals: 3, 
                   blocks: 1, 
                   slam_dunks: 1
+                 },
+                   
+                 {
+                  player_name: "Jeff Adrien",
+                  number: 4, 
+                  shoe: 18,
+                  points: 10, 
+                  rebounds: 1, 
+                  assists: 1, 
+                  steals: 2, 
+                  blocks: 7, 
+                  slam_dunks: 2
                   
                 }, 
+      
+                   
         ]
     },
     away: {
